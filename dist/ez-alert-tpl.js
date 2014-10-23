1218f04212b9f08c2b9e68265b78c5fd92ff4f1c
thebigh2014 <helmut.wollenberg@wardell.biz>
2014-10-23 15:06:30 -0700

    Allow alerts to be dismissed by clicking anywhere on them; removed extraneous ng-click; bumped to 0.1.4

--- dist/ez-alert-tpl.js
+++ dist/ez-alert-tpl.js
@@ -4,13 +4,12 @@ angular.module('ez.alert').run(['$templateCache', function($templateCache) {
   $templateCache.put('ez-alert-tpl.html',
     "<div class=\"ez-alert-container\">\n" +
     "  <div class=\"ez-alert-item\" ng-repeat=\"alert in alerts\" ng-class=\"{'ez-hide': alert.hide}\">\n" +
-    "    <div class=\"{{ alert.alertClass }}\">\n" +
-    "      <a class=\"close\" aria-hidden=\"true\" ng-click=\"hideAlert($index)\">&times;</a>\n" +
+    "    <div class=\"{{ alert.alertClass }}\" ng-click=\"hideAlert($index)\">\n" +
+    "      <a class=\"close\" aria-hidden=\"true\">&times;</a>\n" +
     "      <div ng-bind=\"alert.msg\"></div>\n" +
     "    </div>\n" +
     "  </div>\n" +
-    "</div>\n" +
-    "\n"
+    "</div>\n"
   );
 
 }]);

1ecda365c4a96210041cdacd449d1dd54a14238f
thebigh2014 <helmut.wollenberg@wardell.biz>
2015-01-20 15:52:44 -0800

    Further work on alerts; TODO: make duplicate handling work correctly

--- src/ez-alert.js
+++ src/ez-alert.js
@@ -18,6 +18,12 @@ angular.module('ez.alert', [])
     link: function(scope) {
       scope.$watch(EzAlert.get, function() {
         scope.alerts = EzAlert.get();
+
+        /*scope.$watch('alerts', function(newVal, oldVal) {
+          if (newVal !== oldVal) {
+            console.log(newVal);
+          }
+        }, true);*/
       });
 
       scope.hideAlert = function(index) {
@@ -52,25 +58,17 @@ angular.module('ez.alert', [])
         msg: msg
       };
 
+      var duplicateFound = this.findDuplicates(alertItem, noClear);
+
+      if (!!duplicateFound) {
+        return;
+      }
+
       $interval(function() {
         alertItem.show = true;
-      }, 50, 1);
+      }, 1, 1);
 
-      if (alerts.length >= EzAlertConfig.maxAlerts) {
-        if (EzAlertConfig.insertFirst) {
-          this.hide(alerts[alerts.length - 1]);
-
-          $interval(function() {
-            alerts.pop();
-          }, EzAlertConfig.delay, 1);
-        } else {
-          this.hide(alerts[0]);
-          
-          $interval(function() {
-            alerts.shift();
-          }, EzAlertConfig.delay, 1);
-        }
-      }
+      this.hideExcessAlerts(this, true);
 
       if (EzAlertConfig.insertFirst) {
         alerts.unshift(alertItem);
@@ -82,11 +80,94 @@ angular.module('ez.alert', [])
         this.hide(alertItem, EzAlertConfig.delay);
       }
     },
-    hide: function(alertItem, delay) {
+    findDuplicates: function(newAlert, noClear) {
+      var that = this;
+
+      for (var i = 0, l = alerts.length; i < l; i++) {
+        var alertItem = alerts[i];
+
+        if (alertItem.alertClass === newAlert.alertClass && alertItem.msg === newAlert.msg) {
+          if (!noClear) {
+            alertItem.resetDelay = true;
+          }
+
+          that.hideAndShow(alertItem);
+
+          return true;
+        }
+      }
+
+      return false;
+    },
+    hideExcessAlerts: function(that, repeat) {
+      if (alerts.length >= EzAlertConfig.maxAlerts) {
+        var excessCount = alerts.length - EzAlertConfig.maxAlerts + 1;
+
+        for (var i = 0, l = excessCount; i < l; i++) {
+          if (EzAlertConfig.insertFirst) {
+            that.hide(alerts[alerts.length - (i + 1)], 0, true);
+          }
+          else {
+            that.hide(alerts[i], 0, true);
+          }
+        }
+      }
+
+      if (!repeat) {
+        $interval(function() {
+          that.hideExcessAlerts(that, false);
+        }, 100, 1);
+      }
+    },
+    hide: function(alertItem, delay, immediateRemoval) {
+      var that = this;
+
+      var removalDelay = !!immediateRemoval ? 1000 : (delay + 1000);
+
       $interval(function() {
+        /*if (!!alertItem.resetDelay) {
+          delete alertItem.resetDelay;
+          that.hide(alertItem, delay, immediateRemoval);
+          
+          return;
+        }*/
+
         alertItem.show = false;
         alertItem.hide = true;
       }, delay, 1);
+
+      $interval(function() {
+        if (EzAlertConfig.insertFirst) {
+          alerts.pop();
+        } else {
+          alerts.shift();
+        }
+      }, removalDelay, 1);
+    },
+    hideAndShow: function(alertItem) {
+      var that = this;
+
+      alertItem.show = false;
+      alertItem.hide = true;
+
+      /* Note about fast repetitions of duplicate items:
+       * If duplicates appear in less than 500 ms,
+       * One duplicate will remain on screen.
+       */
+
+      $interval(function() {
+        alertItem.hide = false;
+        alertItem.show = true;
+      }, 500, 1);
+
+      $interval(function() {
+        if (!!alertItem.resetDelay) {
+          delete alertItem.resetDelay;
+          that.hide(alertItem, EzAlertConfig.delay);
+          
+          return;
+        }
+      }, 5, 100);
     }
   };
 }]);
